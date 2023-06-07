483c4f4e24ec7c57ad3c30f423140a1dc708b32f
Monika Kluczyk <monikakluczyk90@gmail.com>
2023-06-07 19:21:54 +0200

    Initial files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const inputAmount = document.querySelector(".input-amount");
 const countingButton = document.querySelector(".btn-count");
 const select = document.querySelector(".select-currency");
 const outputValue = document.querySelector(".output-value");
+const loader = document.querySelector("#loader");
+loader.style.display = "none";
 
 const createURL = () => {
   const selectedCurrency = select.value;
@@ -25,9 +27,16 @@ const getCurrencyCounter = () => {
       } else {
         inputAmount.setCustomValidity("Wpisz poprawną wartość");
       }
+      loader.style.display = "none";
     })
-    .catch(() => (outputValue.textContent = "Wystąpił błąd"));
+    .catch(() => {
+      outputValue.textContent = "Wystąpił błąd";
+      loader.style.display = "none";
+    });
 };
 
 select.addEventListener("change", createURL);
-countingButton.addEventListener("click", getCurrencyCounter);
+countingButton.addEventListener("click", () => {
+  loader.style.display = "block";
+  getCurrencyCounter();
+});
